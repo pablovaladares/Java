781878031ae1681347dac8abc0251f2be4406786
Pablo Valadares <pablopamv@gmail.com>
2022-07-31 12:54:47 -0300

    Teste
    Teste repositório

--- FeiraeFralda/src/main/java/com/mycompany/feiraefralda/FeiraeFralda.java
+++ FeiraeFralda/src/main/java/com/mycompany/feiraefralda/FeiraeFralda.java
@@ -4,14 +4,14 @@ public class FeiraeFralda {
     Feira fe1 = new Feira();
     fe1.local = "Atacadão";
     fe1.formapagamento = "Ticket Alimentação";
-    fe1.valortotal = 299.99f;
+    fe1.valortotal = 300.1f;
     fe1.status();
     fe1.carooubarato();
     
     Fralda fr1 = new Fralda();
     fr1.modelo = "Huggies";
     fr1.quantidade = 48;
-    fr1.tamanho = "Médio";
+    fr1.tamanho = "Grande";
     fr1.status();
     
     }
